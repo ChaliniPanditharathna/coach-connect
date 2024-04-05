77a4604a8f4f5b69bdf6d0dfab76646a1d692a38
nelumkc <rangebandaramuc@student.douglascollege.ca>
2024-04-05 15:04:48 -0700

    Instructor Home page,Approve reject appointments

--- src/services/AppointmentService.js
+++ src/services/AppointmentService.js
@@ -7,6 +7,28 @@ class AppointmentService {
         return http.post("/appointment",data);
     }
     // POST http://localhost:8080/api/login 
+
+    fetchInstructorAppointments(instructorId) {
+        return http.get(`/appointment?instructorId=${instructorId}`);
+      }
+    
+      // Approve a pending appointment
+      approveAppointment(appointmentId) {
+        console.log("Appointment id", appointmentId)
+        return http.put('/appointment/approve', {
+          "appointmentId": appointmentId
+        });
+      }
+
+      //Reject Appointments
+      rejectAppointment(appointmentId, rejectedReason) {
+        console.log("Appointment id", appointmentId)
+        const payload = {
+            appointmentId: appointmentId,
+            rejectedReason: rejectedReason
+        };
+        return http.put('/appointment/reject', payload);
+    }
     
 
 }
